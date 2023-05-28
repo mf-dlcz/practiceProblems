ad0ee2dc96c1b76182bbdaa7efa9c406923dde6e
Maria Delacuz <mar23iaa@gmail.com>
2023-05-27 21:23:50 -0700

    "made some changes to my if statements & I also added more comments to my code explaining what each line is doing"

--- index.js
+++ index.js
@@ -30,40 +30,65 @@
 // if we make it through the full string and arr is empty, return true 
 
 const isValid = (s) => {
+  //I've created two variables that contain an array of chars each
+  let opening = ['(', '{', '[']
+  let closing = [')', '}', ']']
+
+  // Checking to make sure the value passed is not an odd number of characters
+  // By using the length method on the string and diving the length by 2. Its remainder will
+  // let us know if its even or odd. If the remainder is 0 = even; 1 = odd.
   if (s.length % 2 !== 0) {
     return false
   }
-  if (s[0] === ')' || s[0] === '}' || s[0] === ']') {
+  //checks if the chars in s are included in the closing array
+  if (s.includes(closing)) {
     return false
   }
-  if (s[s.length - 1] === '(' || s[s.length - 1] === '{' || s[s.length - 1] === '[') {
+  //checks if the chars in s are included in the opening array
+  if (s.includes(opening)) {
     return false
   }
-
+  // this is an object with keys and value pairs
+  // the key is the opening and the value is the closing
   const pairs = {
     '(': ')',
     '{': '}',
     '[': ']'
   }
-
+  //Empty array where I will eventually .push() items
   let arr = []
 
+  //Traditional For loop specifically for looping through strings 
   for (let i = 0; i < s.length; i++) {
     // if it's an opening bracket, push into stack
     // else, assume it's closing bracket and check if it matches anything
+    //checks string at given index [i] if it's === to the value of the opening array
     if (s[i] === "[" || s[i] === "(" || s[i] === "{") {
+    //each element is being pushed to the empty array created in line 58
       arr.push(s[i])
+      //pairs[given index is removed] !== s at given index [i]
     } else if (pairs[arr.pop()] !== s[i]) {
       console.log(pairs[arr.pop()], s[i])
       return false
     }
   }
   // return arr.length === 0
+  //checks to see if the length of the array is 0 which means it's empty
   if (arr.length === 0) {
     return true
   } else {
     return false
   }
+}
+
+console.log(isValid("(([]})"))
+
+
+  /// ** Bonus:
+  // replace all the || conditions with .includes()
+  // **Hint: create an array with all opening brackets, and also all closing brackets
+  // arr1 = ['[','(', '{']
+  // same w/ closing for arr2
 }
 
 console.log(isValid("(([]})"))
